9b034db681acc6d593776903b0b1178f74061c00
Ashish <abhardwaj0052@gmail.com>
2024-03-22 16:09:51 +0530

    first commit

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+      {/* <h3 className='mt-10 bg-black flex flex-col gap-5 items-center justify-center text-white'>  &copy; by AB.pvt.ltd</h3> */}
+      <div className='text-white bg-black flex p-2 mb-0 justify-around'><h4> About us </h4>
+      <h4> Contact us</h4>
+      <h4>Address : Uttam Nagar , Street - 6 , Delhi , India</h4>
+      </div>
+    </div>
+  )
+}
+
+export default footer
